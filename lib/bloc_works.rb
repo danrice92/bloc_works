51fde5bd0321e2fc1a11e3a7bc4ae9b54f32f6ee
Dan Rice <dan.rice.92@outlook.com>
2017-08-31 12:12:28 -0600

    Architecture Checkpoint 4 completed

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -7,18 +7,12 @@ require "bloc_works/utility"
 module BlocWorks
   class Application
     def call(env)
-      controller_class, action_name = controller_and_action(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        [404, {'Content-Type' => 'text/html'}, ["404 (that's an error). No favicon currently available."]]
-      elsif controller_class == nil || controller_class == false || action_name == nil
-        [404, {'Content-Type' => 'text/html'}, ["404 (that's an error). That path does not exist."]]
-      elsif controller_class.method_defined?(action_name)
-        controller = controller_class.new(env)
-        response_body = controller.send(action_name)
-        return [200, {'Content-Type' => 'text/html'}, [response_body]]
-      else
-        return [404, {'Content-Type' => 'text/html'}, ["404 (that's an error). That path does not exist."]]
+        return [404, {'Content-Type' => 'text/html'}, ["No favicon currently available."]]
       end
+
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
     end
   end
 end
